e4f43f036f9526b5c900e6c2553f9062faeec108
airommel <airommel@users.noreply.github.com>
2024-03-03 16:31:21 +0800

    day2 solution part 1

--- day2/game.js
+++ day2/game.js
@@ -12,6 +12,20 @@ let player = {
     v: 5
 }
 
+function checkCircleCollision(x1, y1, r1, x2, y2, r2) {
+    // 計算兩個圓心之間的距離
+    var distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+    // 判斷兩個圓心距離是否小於兩個圓的半徑和
+    if (distance <= r1 + r2) {
+        // 圓形接觸
+        return true;
+    } else {
+        // 圓形不接觸
+        return false;
+    }
+}
+
 function generateFood() {
     for (let i = 0; i < 5; i++) {
         foods.push({
@@ -25,12 +39,15 @@ function generateFood() {
 function setup() {
     console.log(WIDTH, HEIGHT)
     createCanvas(WIDTH, HEIGHT)
-
+    generateFood()
     // noLoop()
+
 }
 
-generateFood()
+
 function draw() {
+
+
     background(220)
 
 
@@ -48,13 +65,34 @@ function draw() {
         player.y = Math.min(player.y + player.v, HEIGHT - player.d / 2)
     }
 
+    // check if each food collide with player
+    // take food from foods(array of object) one by one 
+    // use checkCollide function with player's xy coordinates input and raidus and food's xy coordinates and radius
+
+    // if collide ,player eats food and grows ,food disappear
+    // if no collide, do nth
+
+    let resultArray = []
+    for (let entry of foods) {
+        // console.log("check entry", entry)
+        let result = checkCircleCollision(player.x, player.y, player.d / 2, entry.x, entry.y, entry.d / 2)
+
+        if (result) {
+            player.d += 10
+            console.log("Collide")
+        } else {
+            resultArray.push(entry)
+        }
+    }
+
+    foods = resultArray
+
 
     fill('red')
     circle(player.x, player.y, player.d)
 
 
     for (let entry of foods) {
-
 
         fill('brown')
         circle(entry.x, entry.y, entry.d)
