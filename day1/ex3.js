7aa29d446cc4e076ecb79ac69074023802828368
airommel <airommel@users.noreply.github.com>
2024-03-03 10:55:27 +0800

    add console to ex3

--- day1/ex3.js
+++ day1/ex3.js
@@ -13,13 +13,22 @@
 function reverseOutput(inputString) {
 
     let result = "";
+    // console.log(`length of string input '${inputString}':`, inputString.length)
     let startIndex = inputString.length - 1
+    // console.log("startIndex of selector:", startIndex, "which is character:", inputString[startIndex])
+    // console.log("loop start:")
     for (let i = 0; i < inputString.length; i++) {
-
+        // console.log("i:", i)
         let targetIndex = startIndex - i
+        // console.log(`targetIndex cal from  ${startIndex} - ${i}:`, targetIndex, ",character:", inputString[targetIndex])
+
         // console.log(inputString[targetIndex])
         result += inputString[targetIndex]
+        // console.log("result after string concat in the loop:", result)
+        // console.log("end of 1 loop")
     }
+
+    // console.log("loop end")
     console.log(result)
 }
 
